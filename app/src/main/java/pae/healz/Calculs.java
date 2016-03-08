75a5ca94bf84b2910d546c5930b94292785bce2f
marionacr <marionacaros@gmail.com>
2016-03-08 13:51:36 +0100

    commit elimino local.properties

---
+++ app/src/main/java/pae/healz/Calculs.java
@@ -0,0 +1,13 @@
+package pae.healz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calculs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculs);
+    }
+}
