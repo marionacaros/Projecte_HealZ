c22e579a4e5e751c3eda375447b4422ee6856f05
Marchez18 <marchez18@gmail.com>
2016-03-10 09:20:14 +0100

    Commit activity_calculs

--- app/src/main/java/pae/healz/Calculs.java
+++ app/src/main/java/pae/healz/Calculs.java
@@ -1,13 +1,33 @@
 package pae.healz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Calculs extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculs);
+        Button_GM();
     }
+
+    private void Button_GM() {
+        button = (Button) findViewById(R.id.button_mesure);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Calculs.this, FC_FR.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
 }
--- app/src/main/java/pae/healz/MainActivity.java
+++ app/src/main/java/pae/healz/MainActivity.java
@@ -1,5 +1,6 @@
 package pae.healz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButtonCalculs();
 
 
 
@@ -37,7 +42,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-//hsckzhvlzsdhv pz  sdibv
+
+    //hsckzhvlzsdhv pz  sdibv
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -77,6 +83,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
             // Handle the camera action
         } else if (id == R.id.nav_history) {
 
@@ -94,4 +102,20 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    private void ButtonCalculs() {
+        button = (Button) findViewById(R.id.button_mesure);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Calculs.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+
 }
