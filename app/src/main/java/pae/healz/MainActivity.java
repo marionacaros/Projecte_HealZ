5afb484a526cf820fd3edb6baa7acd62ecb9a381
Marchez18 <marchez18@gmail.com>
2016-03-29 13:48:47 +0200

    Añadido Perfil sin acabar

--- app/src/main/java/pae/healz/MainActivity.java
+++ app/src/main/java/pae/healz/MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,23 +21,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
 
     // Create new fragment and transaction
     private Fragment frag = new Fragment();
+    private ImageButton button_profile;
 
     //private static final  int TRANSIT_FRAGMENT_OPEN = 4097;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -48,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
+        navigationView.setCheckedItem(R.id.nav_home);
 
     }
 
@@ -64,8 +68,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-
-    //MENU
 
 
     @Override
@@ -87,15 +89,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     public boolean onNavigationItemSelected(MenuItem item) {
+        Log.d("main", "entra onNavigationItemSelected");
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+
 
         if (id == R.id.nav_home) {
             frag= new HomeFragment();
 
         } else if (id == R.id.nav_history) {
-            frag= new HistoryFragment();
-
+            frag = new HistoryFragment();
 
         } else if (id == R.id.nav_settings) {
 
@@ -106,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.nav_send) {
 
         }
+
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, frag)
                 .commit();
@@ -113,6 +117,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         //drawer.closeDrawers();
+
+        button_profile = (ImageButton) findViewById(R.id.ImageHomeProfile);
+        button_profile.setOnClickListener(this);
         return true;
     }
 
@@ -120,4 +127,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
+    @Override
+    public void onClick(View v) {
+        if (v == button_profile){
+            Intent intent = new Intent(MainActivity.this, Perfil.class);
+            startActivity(intent);
+        }
+    }
 }
