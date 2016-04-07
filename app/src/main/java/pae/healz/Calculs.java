2a31aa6453d74e25fe8285206488513f20cca718
Marchez18 <marchez18@gmail.com>
2016-04-07 08:08:54 +0200

    Update abans de seguir

--- app/src/main/java/pae/healz/Calculs.java
+++ app/src/main/java/pae/healz/Calculs.java
@@ -1,16 +1,11 @@
 package pae.healz;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class Calculs extends AppCompatActivity {
 
@@ -56,7 +51,7 @@ public class Calculs extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Calculs.this, Comunication.class);
+                Intent intent = new Intent(Calculs.this, ScanDevices.class);
                 startActivity(intent);
             }
         });
